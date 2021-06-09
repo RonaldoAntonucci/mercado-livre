9a9ecb5721f78367ce70a7f5a468c8f8afb8a0a2
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2021-06-08 21:59:40 -0300

    chore: config port env

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import app from './app';
 
 app.get('/', (req, res) => res.json({ status: 'online' }));
 
-app.listen(3333, () =>
-  console.log(`Server running at http://localhost:${process.env.PORT}`),
+const port = process.env.PORT;
+
+app.listen(port, () =>
+  // eslint-disable-next-line no-console
+  console.log(`Server running at http://localhost:${port}`),
 );
